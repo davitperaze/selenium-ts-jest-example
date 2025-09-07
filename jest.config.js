d8c295b368874aa9c31e72949c29c4dc6ebbcfe2
Davit Peradze <davitperaze@gmail.com>
2025-09-07 16:10:55 +0200

    clean configuration

--- jest.config.js
+++ jest.config.js
@@ -19,14 +19,6 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testMatch: ["**/__tests__/**/*.test.ts"],
-    transform: {
-        '^.+\\.tsx?$': [
-            'ts-jest',
-            {
-                tsconfig: 'tsconfig.json',
-            },
-        ],
-    },
     setupFilesAfterEnv: [],
     reporters: reporters
 };
